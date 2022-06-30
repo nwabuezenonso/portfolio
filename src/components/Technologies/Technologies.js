d53d0097f3584623112fb214536090ef365fc27b
stephen <nwabuezesc@gmail.com>
2022-06-30 09:48:34 +0100

    type correction

--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -30,7 +30,7 @@ const Technologies = () =>  (
           <ListTitle>Back-End</ListTitle>
           <ListParagraph>
             Experience with <br />
-            Node and mongodb <br/> for Databases
+            NodeJs and mongodb <br/> for Databases
           </ListParagraph>
         </ListContainer>
       </ListItem>
